a468057417d83b11747fe7a4d0ad70fe652b4bd0
gharesania <gharesania7173@gmail.com>
2026-02-04 18:18:03 +0530

    Subject Model, Controllers, Routes

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const universityRoutes = require("./routes/universityRoute");
 const programRoutes = require("./routes/programRoute");
 const branchRoutes = require("./routes/branchRoute");
 const semesterRoutes =  require("./routes/semesterRoute");
-
+const subjectRoutes = require("./routes/subjectRoute");
 
 
 // Connect database
@@ -23,6 +23,7 @@ connectDB();
 app.use(express.json());
 app.use(cors());
 
+// Server Connection Testing
 app.get('/', (req, res) => res.send('Hello World!'));
 
 
@@ -31,7 +32,7 @@ app.use("/api/universities", universityRoutes);
 app.use("/api/programs", programRoutes);
 app.use("/api/branches", branchRoutes);
 app.use("/api/semesters", semesterRoutes);
-
+app.use("/api/subjects", subjectRoutes);
 
 
 app.listen(port, () => {
